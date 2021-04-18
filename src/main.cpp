72c580428b413f96ffefc4cd54ebf251627c0282
Mert <mert_ef@freenet.de>
2021-04-18 11:19:09 +0200

    Fixes cause of renamed

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,29 @@
 #include <Arduino.h>
-#include "CWifi.h"
+#include "CWifiBasic.h"
+
+
+//Preset routes are:
+
+// in AP Mode:
+//  "/" for setup WIFI config
+
+// in Normal Mode:
+// "/"        -> Example Root Page
+// "/login"   -> a Basic login form 
+// "/update"  -> OTA Form for firmware update
 
 
 
-CWifi& wifi = CWifi::getInstance();
+CWifiBasic& wifi = CWifiBasic::getInstance();
 #define PIN_WIFI_RESET D0
 
 void setup() {
       Serial.begin(115200);
+      
+      //Init with Reset Pin for reset the WIFI CONFIG 
+      //NOTE - CARE that the pin is set to GND if u dont want to reset !
+      // if pin is not GND the WIFI config will reset
+      // Please let me know if u got a solution for that problem !
       
       wifi.init(PIN_WIFI_RESET);
      
@@ -16,5 +32,5 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-    CWifi::getInstance().run();
+    CWifiBasic::getInstance().run();
 }
