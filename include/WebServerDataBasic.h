abc3ed55f8bada15e05413b4ef06b8f325e62ef3
Mert <mert_ef@freenet.de>
2021-04-18 11:43:28 +0200

    Changed Project Structure

--- include/WebServerDataBasic.h
+++ include/WebServerDataBasic.h
@@ -4,17 +4,17 @@
 
 const char wifisetupwebpage[] = 
 {
-     #include "../data/wifisetupwebpage.html"
+     #include "data/wifisetupwebpage.html"
 };
 
 const char loginIndex[] = 
 {
-    #include "../data/loginIndex.html" 
+    #include "data/loginIndex.html" 
 };
 
 const char updateIndex[] = 
 {
-    #include "../data/updateIndex.html"  
+    #include "data/updateIndex.html"  
 };
 
 
